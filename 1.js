dc87a807cf11d1f12cb54d09e69889f902d561f7
Sri harsha <ansharsha123@gmail.com>
2024-08-08 13:17:42 +0530

    just files

---
+++ 1.js
@@ -0,0 +1,76 @@
+// // // let a=["apple","ball",55,{hero:"kalyan"}]
+// // // // console.log(a)
+// // // // console.log(typeof a)
+// // // // console.log(Array.isArray(a))
+/// // let games={
+// // //     cricket:"dhoni",
+// // //     football:"racer"
+// // // }
+// // // console.log(games)
+// // let a=5;
+// // let b=6;
+// // console.log("Result:",a+b)
+// let a=1;
+// a++
+// console.log(a)
+// let b=5
+// b--
+// console.log(b)
+// let a=0;
+// let b=2;
+// let c=3;
+// if(a==b){
+//     console.log("a=b")
+// }else if (a<b){
+//     console.log("a is small")
+// }
+
+// let a="hero"
+// if (a=="hero"){
+//     console.log("kalyan")
+// }else{
+//     console.log("pavan")
+// }
+
+// prompt("enter your name")
+
+// let woman=confirm("are you a man");
+// if (woman){
+//     let name=prompt("please enter your name");
+//     document.write("hello madam "+name)
+// }else{
+//     let name=prompt("please enter your name");
+//     document.write("hello mr. "+name)
+// }
+
+// let a="55";
+// let b=33;
+// let z=parseInt(a);
+// let x=String(b);
+// console.log(typeof z);
+// console.log(typeof x);
+// let a="5"
+// let b=parseInt(a)
+// console.log(b)
+// console.log(a)
+// let Name=prompt("enter name");
+// let marks=prompt("enter marks");
+// switch(true){
+//     case marks>90 && marks<=100:
+//         result="topper"
+//         break;
+//     default:
+//         result="wrong"
+//         break;
+// }
+// document.write("hello "+Name+"<h1>"+result+"</h1>");
+
+// let a=[1,2,3,4]
+// a.push(1,2);
+// console.log(a);
+
+let a=["1","2","3"]
+let b=a.pop()
+console.log(b)
+console.log(a)
+
